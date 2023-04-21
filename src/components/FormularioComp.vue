b787caf399c1c0d029bca198788e5515791d2c3b
Jonathaneche <jonathan.eche@gmail.com>
2023-04-21 12:28:28 +0200

    crud completo

--- src/components/FormularioComp.vue
+++ src/components/FormularioComp.vue
@@ -1,19 +1,21 @@
 <template>
     <div>
-        <h2>Lista de tareas</h2>
-        <form @submit.prevent>
-            <input v-model="formData.titulo" type="text" placeholder="Ej. Ir al banco">
-            <input v-model="formData.descripcion" type="text" placeholder="Ej. Estar en la oficina a las 8am ">
-            <input v-model="formData.quien" type="text" placeholder="Gorka">
-        </form>
-        <button @click="agregarTarea">Agregar</button>
-    
+        <img v-if="isError" src="../assets/logo.png" alt="">
+        <div v-if="!isError">
+                <h2>Lista de tareas</h2>
+                <form @submit.prevent>
+                    <input v-model="formData.titulo" type="text" placeholder="Ej. Ir al banco">
+                    <input v-model="formData.descripcion" type="text" placeholder="Ej. Estar en la oficina a las 8am ">
+                    <input v-model="formData.quien" type="text" placeholder="Gorka">
+                </form>
+            <button @click="agregarTarea">Agregar</button>
+        </div> 
 
     </div>
 </template>
 
 <script setup>
-import { reactive} from "vue"
+import {reactive} from "vue"
 import axios from "axios"
 
 let formData = reactive({
@@ -25,26 +27,18 @@ let formData = reactive({
 
 function agregarTarea(){
     let isError = false;
-
     try{
         axios.post("http://localhost:3000/tareas", {
             titulo: formData.titulo,
             descripcion: formData.descripcion,
             quien: formData.quien
         })
-       
-        
-    }
-    catch(error){
+        location.reload()
+    }catch (error){   
         isError = true;
     }
-
-    location.reload()
+    return isError
 }
-
-
-
-
 
 </script>
 
