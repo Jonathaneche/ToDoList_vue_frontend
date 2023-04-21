321d474efdd9d9fa1a17ca335626c14bdb6d31d4
Jonathaneche <jonathan.eche@gmail.com>
2023-04-21 10:00:59 +0200

    modificacion de componentes lista y formulario

--- src/components/ListaComp.vue
+++ src/components/ListaComp.vue
@@ -1,10 +1,20 @@
 <template>
-
-
-
+    <h2>Lista de tareas</h2>       
+            <div v-for="tarea in infoData.listaDeTareas" :key="tarea.id">
+                <p>{{ tarea.titulo }}</p>
+                <p>{{ tarea.descripcion}}</p>
+                <p>{{ tarea.quien}}</p>
+            </div>
 </template>
 
 <script setup>
+
+import { defineProps } from 'vue';
+
+const prop = defineProps({
+    infoData: Object
+})
+
 
 
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <img src="../img/isloading.gif" v-if="isLoading" alt="Gif cuando la pagina carga">
-        <img src="../img/error.gif" alt="Gif si no carga la informacion">
-        <div v-if="!isLoading && !isError">
-            <FormularioComp :infoData="infoData"/>
-            <ListaComp />
+        <img src="../img/isloading.gif" v-if="infoData.isLoading" alt="Gif cuando la pagina carga">
+        <img v-if="infoData.isError" src="../img/error.gif" alt="Gif si no carga la informacion">
+        <div v-if="!infoData.isLoading && !infoData.isError">
+            <FormularioComp />
+            <ListaComp :infoData="infoData" />
         </div>
 
 
@@ -17,21 +17,15 @@
 import FormularioComp from "@/components/FormularioComp.vue";
 import ListaComp from "@/components/ListaComp.vue"
 
-import { defineProps } from 'vue';
-
 import {ref, onMounted} from 'vue';
 import info from "../dataInfo/dataInfo.js"
 
-let infoData = ref(onMounted(async ()=> {
-    infoData.value = info.obtenerDatos();
+let infoData = ref(onMounted(async () => {
+    infoData.value = await info.obtenerDatos();
 }))
 
-console.log(info)
+console.log("**", infoData)
 
-defineProps({
-  infoData: Object,
-
-});
   
 </script>
 
