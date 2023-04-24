2005e64cc7cf5fe7e40b5fc58d877e3076652f47
Jonathaneche <jonathan.eche@gmail.com>
2023-04-24 08:48:20 +0200

    falta los estilos

--- src/components/ListaComp.vue
+++ src/components/ListaComp.vue
@@ -1,20 +1,27 @@
 <template>
-    <h2>Lista de tareas</h2>       
-            <div v-for="tarea in infoData.listaDeTareas" :key="tarea.id">
-                <p>{{ tarea.titulo }}</p>
-                <p>{{ tarea.descripcion}}</p>
-                <p>{{ tarea.quien}}</p>
+    <h2 class="titulo_lista_tareas">Lista de tareas</h2>   
+        <div class="contenedor_tareas">
+            
+            <div
+                class="card_tarea"
+                v-for="tarea in infoData.listaDeTareas" 
+                :key="tarea.id">
+                <p><strong>Titulo:</strong> {{ tarea.titulo }}</p>
+                <p><strong>Descripcion:</strong> {{ tarea.descripcion}}</p>
+                <p><strong>Hora:</strong> {{ tarea.quien}}</p>
                 <div>
                     <button @click="editarTarea(tarea.id, tarea.quien)">Editar</button>
                     <button @click="elimilarTarea(tarea.id)">Eliminar</button>
                 </div>
             </div>
-
             <div v-show="mostrarFormulario">
                 <input v-model="formTitulo" type="text" placeholder="Ej. Ir al banco">
                 <input v-model="formDescripcion" type="text" placeholder="Ej. Estar en la oficina a las 8am ">
                 <button @click="actualizarBD">Actualizar</button>
             </div>
+        </div>
+
+
 </template>
 
 <script setup>
@@ -71,5 +78,23 @@ defineProps({
 </script>
 
 <style scoped>
+    .titulo_lista_tareas {
+        text-align: center;
+    }
+    .contenedor_tareas{
+        background-color: cadetblue;
+        width: 90vw;
+        margin: 0 auto;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        padding: 1rem;
+        gap: 1.5rem;
 
+    }
+
+    .card_tarea{
+        background-color: bisque;
+        padding: 1rem;
+        
+    }
 </style>
